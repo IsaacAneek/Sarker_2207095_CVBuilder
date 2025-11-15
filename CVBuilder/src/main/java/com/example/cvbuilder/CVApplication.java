e546a981ea029bcbd9db4910d5f21f0f07ef6517
Isaac <81284474+IsaacAneek@users.noreply.github.com>
2025-11-15 11:34:21 +0600

    Updated create.fxml and added further labels and fields

--- CVBuilder/src/main/java/com/example/cvbuilder/CVApplication.java
+++ CVBuilder/src/main/java/com/example/cvbuilder/CVApplication.java
@@ -7,12 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class CVApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 480, 640);
         stage.setTitle("Hello!");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
